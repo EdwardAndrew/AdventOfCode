a15306ebdbe2748b56868f61aa8840f2ef619846
Ed Andrew <ed.andrew95@gmail.com>
2020-12-07 16:38:17 +0000

    solve day 7 part two

--- 2020/7/main.js
+++ 2020/7/main.js
@@ -5,52 +5,50 @@ class DirectedGraph {
     verticies = {};
 
     addVertex(v) {
-        this.verticies[v] = [];
+        if (!this.verticies[v]) this.verticies[v] = [];
     }
 
     getVertex(v) {
-        return this.verticies[v];
+        return this.verticies[v] || [];
     }
 
     addEdge(a, b, n) {
-        this.verticies[a].push({ bag: b, value: n });
-    }
-
-    depthFirstSearch(start, target) {
-        const path = [start];
-        const visited = new Set([start]);
-        let currentNode = start;
-        while (path[path.length - 1] != target && path.length > 0) {
-            let foundOutlet = false;
-            let visitNode = '';
-            for (let i = 0; i < this.verticies[currentNode].length; i++) {
-                const n = this.verticies[currentNode][i];
-                if (!visited.has(n.bag)) {
-                    foundOutlet = true;
-                    visitNode = n.bag;
-                    if (n.bag == target) return path;
-                }
-            }
-            if (foundOutlet) {
-                path.push(visitNode)
-                visited.add(visitNode)
-            } else {
-                path.pop();
-            }
-            currentNode = path[path.length - 1];
+        if (!this.verticies[a]) this.addVertex(a);
+        if (!this.verticies[a].find(x => x.bag == b)) {
+            this.verticies[a].push({ bag: b, value: n });
         }
-
-        return [];
     }
 }
 
-const buildGraph = entries => {
+const buildGraphOne = entries => {
     const graph = new DirectedGraph();
     const numRegExp = new RegExp(/\d+/);
-    const noOtherRegExp = new RegExp(/no other/g);
+    const noOtherRegExp = new RegExp(/other/);
 
-    entries.map(e => {
-        const s = e.split(' bags contain ');
+    entries.forEach(e => {
+        const s = e.split(' bags contain');
+        graph.addVertex(s[0]);
+        s[1]
+            .replace(/(bags?|\.)/g, '')
+            .split(',')
+            .filter(x => !noOtherRegExp.test(x))
+            .forEach((b => {
+                const num = parseInt(b.match(numRegExp));
+                const name = b.replace(numRegExp, '').trim();
+                graph.addEdge(name, s[0], num)
+            }));
+    });
+
+    return graph;
+}
+
+const buildGraphTwo = entries => {
+    const graph = new DirectedGraph();
+    const numRegExp = new RegExp(/\d+/);
+    const noOtherRegExp = new RegExp(/no other/);
+
+    entries.forEach(e => {
+        const s = e.split(' bags contain');
         graph.addVertex(s[0]);
         s[1]
             .replace(/(bags?|\.)/g, '')
@@ -66,23 +64,46 @@ const buildGraph = entries => {
     return graph;
 }
 
-const partOne = () => {
-    const entries = getEntries();
-    const g = buildGraph(entries);
-    return entries.reduce((acc, current) => {
-        const path = g.depthFirstSearch(current.split(' bags contain ')[0], 'shiny gold');
-        if (path.length > 0) return acc + 1;
-        return acc;
-    }, 0);
-};
+const partOne = s => {
+    const g = buildGraphOne(getEntries());
 
-const partTwo = () => {
-    const entries = getEntries();
-    const g = buildGraph(entries);
+    const path = [s];
+    const visited = new Set([s]);
+    let currentNode = s;
+    while (path.length > 0) {
+        let foundOutlet = false;
+        let visitNode = '';
+        for (let i = 0; i < g.getVertex(currentNode).length; i++) {
+            const n = g.getVertex(currentNode)[i];
 
-    console.log(g.getVertex('shiny gold'))
+            if (!visited.has(n.bag)) {
+                foundOutlet = true;
+                visitNode = n.bag;
+            }
+        }
+        if (foundOutlet) {
+            path.push(visitNode);
+            visited.add(visitNode)
+        } else {
+            path.pop();
+        }
+        currentNode = path[path.length - 1];
+    }
+
+    return visited.size - 1;
 }
 
-console.log(partOne());
-console.log(partTwo());
+const partTwo = s => {
+    const g = buildGraphTwo(getEntries());
+    let result = 0;
 
+    for (let i = 0; i < g.getVertex(s).length; i++) {
+        const child = g.getVertex(s)[i];
+        result += child.value * (1 + partTwo(child.bag))
+    }
+
+    return result;
+}
+
+console.log(partOne('shiny gold'));
+console.log(partTwo('shiny gold'));
